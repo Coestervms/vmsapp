cd3018f8e0a310f20a3386d6f82b12cb7d338db9
Coestervms <umer.farooq@re2qa.com>
2016-07-20 16:04:41 +0500

    Add files via upload

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+	echo "Connected successfully";
+}
+?>
